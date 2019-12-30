4d8791c34370b625d7f7983f369ed36b53562cc2
Dave-Logan <desetro@hotmail.com>
2019-12-29 16:12:58 -0800

    Added 7 day forecast

--- David Weather App.py
+++ David Weather App.py
@@ -2,10 +2,21 @@ import requests
 import json
 x = 39.7456
 y = -97.0892
-    #x = input('First Coordinate: ')
-    #y = input('Second Coordinate: ')
+#x = input('First Coordinate: ')
+#y = input('Second Coordinate: ')
 r = requests.get('https://api.weather.gov/points/' + str(x) + ',' + str(y))
-    #print(r.json()['properties']['forecast']) - used to check if the data is worked
+#print(r.json()['properties']['forecast']) - used to check if the data is worked
 weather = r.json()['properties']['forecast']
 output = requests.get(weather)
-print(output.json())
+#print(output.json()['properties']['periods']) - used to check if logic works
+for forecast in output.json()['properties']['periods']:
+    if  forecast['isDaytime'] == True:
+        print(forecast['name'])
+        print('current temperature'+ ' ' + str(forecast['temperature'])+' '+forecast['temperatureUnit'])
+        print(forecast['detailedForecast'])
+        print('')
+    else:
+        continue
+
+    #print(forecast['name'])
+
